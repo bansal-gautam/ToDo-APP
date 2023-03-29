c74baa9628b8b9c1bca2934557a7f01479bd2274
Gautam Bansal <119778021+bansal-gautam@users.noreply.github.com>
2023-03-29 21:02:09 +0530

    Added Initial Files

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+let items = [];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    let day = date();
+    res.render('list', { 
+        listTitle: day,
+        newListItems: items 
+    });
+});
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    if(req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/work", (req, res) => {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server Started at port 3000");
+});
